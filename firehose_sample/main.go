97b0ab9ecec6b838a20c8cdbf6afb0ead4ec637e
Your Name <you@example.com>
2014-10-16 10:11:56 -0600

    firehose_sample provides subscription_id to NOAA
    [#80129308]

--- firehose_sample/main.go
+++ firehose_sample/main.go
@@ -7,15 +7,16 @@ import (
 	"os"
 )
 
-var DopplerAddress = "wss://doppler.10.244.0.34.xip.io:443"
 var authToken = os.Getenv("CF_ACCESS_TOKEN")
+const DopplerAddress = "wss://doppler.10.244.0.34.xip.io:443"
+const firehoseSubscriptionId = "firehose-a"
 
 func main() {
 	connection := noaa.NewNoaa(DopplerAddress, &tls.Config{InsecureSkipVerify: true}, nil)
 	connection.SetDebugPrinter(ConsoleDebugPrinter{})
 
 	fmt.Println("===== Streaming Firehose (will only succeed if you have admin credentials)")
-	msgChan, err := connection.Firehose(authToken)
+	msgChan, err := connection.Firehose(firehoseSubscriptionId, authToken)
 
 	if err != nil {
 		fmt.Printf("===== Error streaming: %v\n", err)
@@ -24,7 +25,6 @@ func main() {
 			fmt.Printf("%v \n", msg)
 		}
 	}
-
 }
 
 type ConsoleDebugPrinter struct{}
